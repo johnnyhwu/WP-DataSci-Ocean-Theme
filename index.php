f20c5fff0b2d047b618a4b3a488c6f7b53dcf74c
johnnyhwu <johnny@Johnnys-MacBook-Air.local>
2022-01-21 16:19:08 +0800

    finish home page (index.php)

--- index.php
+++ index.php
@@ -7,41 +7,76 @@
     <div class="banner">
         <div class="left">
             <div class="site-title">
-                DataSci Ocean
+                <?php echo get_bloginfo('name'); ?>
             </div>
 
             <div class="site-desc">
-                你也對資料科學深感興趣嗎？那你來對了地方！我們透過文章與影音的形式分享眾多主題的知識，包含程式語言、機器學習、深度學習、統計分析與雲端運算。讓我們一步又一步奠定基礎，享受學習的樂趣吧！
+                <?php echo get_field('site_desc'); ?>
             </div>
 
             <div class="site-btn">
-                <a href="#"><span>開始學習</span></a>
+                <a href="<?php echo get_field('site_btn')['url']; ?>"><span><?php echo get_field('site_btn')['title']; ?></span></a>
             </div>
         </div>
 
         <div class="right">
-            <div class="youtube-player" data-id="eJCXFIoOwdw"></div>
+            <div class="youtube-player" data-id="<?php echo get_field('youtube'); ?>"></div>
         </div>
     </div>
 
     <div class="sub-container">
         <div class="left">
 
-          <?php for($i=0; $i<10; $i++): ?>
-              <article>
-                  <div class="article-info">
-                      <div class="text">
-                        <div class="title">這是文章的標題</div>
-                        <div class="abstract">這是文章的摘要哇哇哇哇～這是一篇範例文章～～～超級酷的！！！</div>
-                      </div>
-                      <div class="meta-data">
-                          <span class="date">Jan 11</span>．<span class="estimation">11 min read</span>．<span class="category">Python 教學</span>
-                      </div>
-                  </div>
+            <?php
+                $args = array(
+                    'numberposts' => 10
+                );
+                
+                $latest_posts = get_posts($args);
+            ?>
 
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/sample-image.jpeg" alt="article thumbnail">
-              </article>
-          <?php endfor; ?>
+            <?php foreach($latest_posts as $post): ?>
+                <article>
+                    <div class="article-info">
+                        <div class="text">
+                            <div class="title"><?php echo $post->post_title ?></div>
+                            <div class="abstract"><?php echo get_field('abstract', $post->ID); ?></div>
+                        </div>
+
+                        <div class="meta-data">
+                            <?php 
+                                $cate = get_the_category($latest_posts[0]->ID)[0];
+                                $cate_name = $cate->name;
+                                $cate_link = get_category_link($cate->term_id);
+                            ?>
+                            <span class="date"><?php echo date("M j", strtotime($post->post_date)); ?></span>．<span class="estimation"><?php echo get_field('estimated_time', $post->ID); ?> min read</span>．<span class="category"><a href="<?php echo $cate_link; ?>"><?php echo $cate_name; ?></a></span>
+                        </div>
+                    </div>
+
+                    <img src="<?php echo get_field('thumbnail', $post->ID)['sizes']['medium'] ?>" alt="article thumbnail">
+                </article>
+            <?php endforeach; ?>
+ 
+
+            <?php for($i=0; $i<10; $i++): ?>
+
+                <!-- 
+                article template: 
+                <article>
+                    <div class="article-info">
+                        <div class="text">
+                            <div class="title">這是文章的標題</div>
+                            <div class="abstract">這是文章的摘要哇哇哇哇～這是一篇範例文章～～～超級酷的！！！</div>
+                        </div>
+                        <div class="meta-data">
+                            <span class="date">Jan 11</span>．<span class="estimation">11 min read</span>．<span class="category">Python 教學</span>
+                        </div>
+                    </div>
+
+                    <img src="<?php /* echo get_template_directory_uri();*/ ?>/assets/images/sample-image.jpeg" alt="article thumbnail">
+                </article>
+            -->
+            <?php endfor; ?>
         </div>
 
         <div class="right">
@@ -60,13 +95,21 @@
             <div class="box-divider"></div>
 
             <div class="multichannel-box">
-                MULTICHANNEL BOX
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube-logo.png" alt="youtube"></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram-logo.png" alt="instagram"></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/matters-logo.jpeg" alt="matters"></a>
             </div>
 
             <div class="box-divider"></div>
 
             <div class="ad-box">
-                AD BOX
+                <div>
+                    AD1
+                </div>
+
+                <div>
+                    AD2
+                </div>
             </div>
         </div>
     </div>
