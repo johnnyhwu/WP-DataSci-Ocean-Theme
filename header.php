64b519cb4de02197f2679b89b89bad10fe2aff60
johnnyhwu <l066858998@gmail.com>
2022-02-21 11:27:47 +0800

    add meta desc

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="<?php echo get_field('meta_desc'); ?>">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
