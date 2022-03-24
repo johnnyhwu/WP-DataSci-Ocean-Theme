0a8b429afef6659e6e634e672069c9f1c239ffc3
johnnyhwu <l066858998@gmail.com>
2022-03-24 14:46:31 +0800

    edit

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html" />
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo get_field('meta_desc', get_queried_object()); ?>">
