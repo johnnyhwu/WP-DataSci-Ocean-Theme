3cb3990cd21ebd9526fbfd62419e76e13d50cfe4
johnnyhwu <l066858998@gmail.com>
2022-03-24 14:50:16 +0800

    add X-Content-Type-Options meta

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta http-equiv="Content-Type" content="text/html" />
+    <meta http-equiv="X-Content-Type-Options" content="nosniff">
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo get_field('meta_desc', get_queried_object()); ?>">
