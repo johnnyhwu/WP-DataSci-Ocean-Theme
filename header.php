63af003ff5a9674ed5fe16c45e00aab0cd16a460
johnnyhwu <l066858998@gmail.com>
2022-06-18 02:03:24 +0000

    embed youtube in singlw post

--- header.php
+++ header.php
@@ -298,7 +298,55 @@
 
 <script>
 
+    /*
+    * Light YouTube Embeds by @labnol
+    * Credit: https://www.labnol.org/
+    */
+
+    function labnolIframe(div) {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute(
+            'src',
+            'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0'
+        );
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allowfullscreen', '1');
+        iframe.setAttribute(
+            'allow',
+            'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+        );
+        div.parentNode.replaceChild(iframe, div);
+    }
+
+    function initYouTubeVideos() {
+        var playerElements = document.getElementsByClassName('youtube-player');
+        console.log("TEST");
+        for (var n = 0; n < playerElements.length; n++) {
+            var videoId = playerElements[n].dataset.id;
+            var div = document.createElement('div');
+            div.setAttribute('data-id', videoId);
+            var thumbNode = document.createElement('img');
+            thumbNode.alt = 'youtube video'
+            thumbNode.src = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace(
+            'ID',
+            videoId
+            );
+            div.appendChild(thumbNode);
+            var playButton = document.createElement('div');
+            playButton.setAttribute('class', 'play');
+            div.appendChild(playButton);
+            div.onclick = function () {
+            labnolIframe(this);
+            };
+            playerElements[n].appendChild(div);
+        }
+    }
+
     window.onload = function insertGoogleADs() {
+
+        // display youtube video
+        initYouTubeVideos();
+
         // create google ads script after whole page is loaded for 5 seconds
         var script = document.createElement('script');
         script.async = true;
@@ -331,8 +379,19 @@
             }
         }, 1000);
 
-        
+        // display disqus section
+        setTimeout(function loadDisqus() {
+            var disqus_config = function () {
+                this.page.url = "<?php echo get_permalink(); ?>";  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = "<?php echo get_the_ID(); ?>"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            };
 
-        initYouTubeVideos();
+            (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://datasci-ocean.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        }, 3500);
     }    
 </script>
--- single.php
+++ single.php
@@ -245,6 +245,47 @@
 
                         margin: 0;
                     }
+                    
+                div.middle-content section.current-post div.youtube-player {
+                    width: 100%;
+                    height: 100%;
+
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                }
+
+                    div.middle-content section.current-post div.youtube-player > div {
+                        position: relative;
+
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                    }
+
+                        div.middle-content section.current-post div.youtube-player > div > img {
+                            max-width: 100%;
+                            max-height: 100%;
+                        }
+
+                        div.middle-content section.current-post div.youtube-player > div > div.play {
+                            position: absolute;
+
+                            width: 15%;
+                            height: 15%;
+
+                            background-image: url('//i.imgur.com/TxzC70f.png');
+                            background-repeat: no-repeat;
+                            background-size: contain;
+                            background-position: center;
+                        }
+
+                        div.middle-content section.current-post div.youtube-player > iframe {
+                            width: 100%;
+                            height: 360px;
+                        }
             
             section.discussion {
                 margin-top: 7vh;
@@ -306,21 +347,5 @@
 </style>
 
 <script>
-    window.onload = function singlePostLoaded() {
 
-        setTimeout(function loadDisqus() {
-            var disqus_config = function () {
-                this.page.url = "<?php echo get_permalink(); ?>";  // Replace PAGE_URL with your page's canonical URL variable
-                this.page.identifier = "<?php echo get_the_ID(); ?>"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-            };
-
-            (function() { // DON'T EDIT BELOW THIS LINE
-                var d = document, s = d.createElement('script');
-                s.src = 'https://datasci-ocean.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-            })();
-        }, 3500);
-
-    }
 </script>
