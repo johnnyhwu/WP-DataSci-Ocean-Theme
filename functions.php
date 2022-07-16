ee525be16d0912fca3b7f6d9ed1d8d97d0e26a1e
johnnyhwu <l066858998@gmail.com>
2022-07-16 07:56:24 +0000

    remove gutenberg css

--- functions.php
+++ functions.php
@@ -14,6 +14,14 @@ function import_css() {
 }
 add_action('wp_enqueue_scripts', 'import_css');
 
+/* dequeue Gutenberg css file */
+function remove_wp_block_library_css() {
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
+
 
 /* add custom post type */
 function add_tool_post_type() {
